f33f154fcb218ada9f4356cbb01ec421aa334b54
Kashingi <moriskashing74@gmail.com>
2026-06-11 19:29:38 +0300

    Added fixes on package naming

--- src/main/java/com/netflix/com/netflix/security/JwtAuthenticationFilter.java
+++ src/main/java/com/netflix/com/netflix/security/JwtAuthenticationFilter.java
@@ -22,7 +22,7 @@ import java.util.Collections;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
-    private JwtUtil jwtUtil;
+    private com.netflix.security.JwtUtil jwtUtil;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
